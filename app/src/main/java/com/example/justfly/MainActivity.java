eed9e997cc59ff4add0992ae287b1cc491a63cf5
Vidovic Branislav <branislav.vidovic@ext.a1.at>
2024-07-24 18:35:58 +0200

    add pause/resume for saving config

--- app/src/main/java/com/example/justfly/MainActivity.java
+++ app/src/main/java/com/example/justfly/MainActivity.java
@@ -17,27 +17,58 @@ import org.osmdroid.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String preferenceFileName = getPackageName() + "_preferences";
-        SharedPreferences sharedPreferences = getSharedPreferences(preferenceFileName, Context.MODE_PRIVATE);
-        Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx, sharedPreferences);
+        loadPreferences();
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         setupInsets();
 
-        MapView map = findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.getController().setZoom(10.0);
-        map.setMinZoomLevel(5.0);
-        map.setMultiTouchControls(true);
-        map.setTilesScaledToDpi(true);
+        mapView = findViewById(R.id.map);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getController().setZoom(10.0);
+        mapView.setMinZoomLevel(5.0);
+        mapView.setMultiTouchControls(true);
+        mapView.setTilesScaledToDpi(true);
         GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
-        map.getController().setCenter(startPoint);
+        mapView.getController().setCenter(startPoint);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadPreferences();
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        savePreferences();
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+
+    private void savePreferences() {
+        String preferenceFileName = getPackageName() + "_preferences";
+        SharedPreferences sharedPreferences = getSharedPreferences(preferenceFileName, Context.MODE_PRIVATE);
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().save(ctx, sharedPreferences);
+    }
+
+    private void loadPreferences() {
+        String preferenceFileName = getPackageName() + "_preferences";
+        SharedPreferences sharedPreferences = getSharedPreferences(preferenceFileName, Context.MODE_PRIVATE);
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, sharedPreferences);
     }
 
     private void setupInsets() {
