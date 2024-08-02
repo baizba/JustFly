ad0aac7997fc94475fe24ad238b078e0f81300d6
Vidovic Branislav <branislav.vidovic@ext.a1.at>
2024-08-02 14:49:01 +0200

    direction-line: code review

--- app/src/main/java/com/example/justfly/overlay/DirectionLineOverlay.java
+++ app/src/main/java/com/example/justfly/overlay/DirectionLineOverlay.java
@@ -14,41 +14,49 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class DirectionLineOverlay extends Overlay {
 
-    private static final String TAG = "DirectionLineOverlay";
+    private static final float LINE_LENGTH = 600.0f;
+    private static final float STROKE_WIDTH = 5.0f;
+
     private final MyLocationNewOverlay myLocationNewOverlay;
+    private final Paint paint;
 
     public DirectionLineOverlay(MyLocationNewOverlay myLocationNewOverlay) {
         this.myLocationNewOverlay = myLocationNewOverlay;
+        this.paint = createPaint();
     }
 
     @Override
     public void draw(Canvas canvas, MapView mapView, boolean shadow) {
         GeoPoint currentLocation = myLocationNewOverlay.getMyLocation();
-
-        // The direction the user is facing
         Location lastFix = myLocationNewOverlay.getLastFix();
 
-        if (currentLocation == null || !lastFix.hasBearing()) {
-            return;
+        if (currentLocation == null || lastFix == null || !lastFix.hasBearing()) {
+            return; // If there is no location or bearing, do not draw the line
         }
 
-        Point screenPoint = new Point();
-        mapView.getProjection().toPixels(currentLocation, screenPoint);
+        Point userPointOnScreen = new Point();
+        mapView.getProjection().toPixels(currentLocation, userPointOnScreen);
 
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(5.0f);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setAntiAlias(true);
+        drawDirectionLine(canvas, userPointOnScreen, lastFix.getBearing());
+    }
 
+    private void drawDirectionLine(Canvas canvas, Point userPointOnScreen, float bearing) {
         Path path = new Path();
-        path.moveTo(screenPoint.x, screenPoint.y);
-        path.lineTo(screenPoint.x, screenPoint.y - 600);
+        path.moveTo(userPointOnScreen.x, userPointOnScreen.y);
+        path.lineTo(userPointOnScreen.x, userPointOnScreen.y - LINE_LENGTH);
 
-        //save and restore to restore original canvas rotation (check javadoc of these 2 methods)
         canvas.save();
-        canvas.rotate(lastFix.getBearing(), screenPoint.x, screenPoint.y);
+        canvas.rotate(bearing, userPointOnScreen.x, userPointOnScreen.y);
         canvas.drawPath(path, paint);
         canvas.restore();
+    }
+
+    private Paint createPaint() {
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(STROKE_WIDTH);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        return paint;
     }
 }
