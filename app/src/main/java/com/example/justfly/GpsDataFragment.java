64b5531ef1729e9e786a9d7bf2046f42cc38ba9f
b.vidovic <b.vidovic@jit.at>
2026-09-15 22:37:40 +0200

    notification works again

--- app/src/main/java/com/example/justfly/GpsDataFragment.java
+++ app/src/main/java/com/example/justfly/GpsDataFragment.java
@@ -1,7 +1,10 @@
 package com.example.justfly;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.justfly.gps.GpsController;
@@ -39,6 +44,7 @@ public class GpsDataFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        requestNotificationPermission();
         View view = inflater.inflate(R.layout.fragment_gps_data, container, false);
         view.findViewById(R.id.infoButton).setOnClickListener(v -> showInfoDialog());
         view.findViewById(R.id.btnViewGpx).setOnClickListener(v -> showGpxDialog());
@@ -69,6 +75,20 @@ public class GpsDataFragment extends Fragment {
 
     private Intent getGpxRecordingServiceIntent() {
         return new Intent(requireContext(), GpxRecordingService.class);
+    }
+
+    private void requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+                && ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                    100
+            );
+        }
     }
 
 }
